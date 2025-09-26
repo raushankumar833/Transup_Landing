154de9749f2a89a9c306e09c72b02c889a13754d
Devgupta1312 <mr.devgupta1302@gmail.com>
2025-09-26 18:22:49 +0530

    chnages

--- src/layouts/main/Header.tsx
+++ src/layouts/main/Header.tsx
@@ -81,7 +81,7 @@ export default function Header() {
             variant="contained"
             rel="noopener"
             //  href={PATH_AUTH.login}
-            onClick={() => (window.location.href = 'https://app.Transup.in/login')}
+            onClick={() => (window.location.href = 'https://app.Transup.in/qrlogin')}
             sx={{
               backgroundColor: '#fff',
               fontSize: isDesktop ? '14px' : '10px',
